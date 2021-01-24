d5e5647bf7b4aacc2ec99eee5b087410414d6f36
TaYaKi71751 <shantian.tw@gmail.com>
2021-01-24 19:27:24 +0000

    Use checkra1n 1.12.1 beta

--- install-arm64.sh
+++ install-arm64.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo mkdir -p /opt/cumbox
 sudo chmod 777 /opt/cumbox
-wget https://assets.checkra.in/downloads/linux/cli/arm64/b48774e5d240ce192016a3fa97df7ef855220576f0704c83ed627d092cb2e224/checkra1n -O /opt/cumbox/checkra1n
+wget https://assets.checkra.in/downloads/linux/cli/arm64/11c0a214dc0fc19050a8103ed5b0ebf4874df3191e34491e177f3af3baced0e7/checkra1n -O /opt/cumbox/checkra1n
 sudo chmod +x /opt/cumbox/checkra1n
 sudo cp 01-cumbox.rules /etc/udev/rules.d
 echo "cumbox is now installed!"
